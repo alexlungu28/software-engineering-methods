22bdb22fb1aae29aa1a901bb1a592e7adbdf1762
Nikolaos Efthymiou <n.efthymiou@student.tudelft.nl>
2020-11-25 14:19:55 +0200

    add Room Repository

---
+++ rooms/src/main/java/op29sem58/room/repositories/RoomRepository.java
@@ -0,0 +1,11 @@
+package op29sem58.room.repositories;
+
+import op29sem58.room.entities.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Integer> {
+
+}
+
