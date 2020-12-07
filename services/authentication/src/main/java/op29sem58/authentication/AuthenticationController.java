56ee6d511dac95b0cf20a43e655caa4e08409a35
Alexandru Lungu <A.Lungu@student.tudelft.nl>
2020-12-07 19:03:42 +0200

    Add endpoints to verify user role and username

--- services/authentication/src/main/java/op29sem58/authentication/AuthenticationController.java
+++ services/authentication/src/main/java/op29sem58/authentication/AuthenticationController.java
@@ -1,14 +1,13 @@
 package op29sem58.authentication;
 
+import io.jsonwebtoken.Jwts;
 import op29sem58.authentication.users.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AuthenticationController {
@@ -44,5 +43,26 @@ public class AuthenticationController {
 
         final String jwt = jwtTokenUtil.generateToken(userDetails);
         return jwt;
+    }
+
+    @GetMapping("/isStudent")
+    public boolean isStudent() {
+        return true;
+    }
+
+    @GetMapping("/isTeacher")
+    public boolean isTeacher() {
+        return true;
+    }
+
+    @GetMapping("/isAdmin")
+    public boolean isAdmin() {
+        return true;
+    }
+
+    @GetMapping("/getUsername")
+    public String getUsername(@RequestHeader("Authorization") String token) {
+        token = token.replace("Bearer ", "");
+        return Jwts.parser().setSigningKey("secret").parseClaimsJws(token).getBody().getSubject();
     }
 }
