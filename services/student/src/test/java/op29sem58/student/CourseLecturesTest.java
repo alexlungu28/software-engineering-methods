e89e07df988a04c6ace6f6df68a76d58cf7ac86c
michelbulten <m.j.g.bulten@student.tudelft.nl>
2020-12-07 14:24:10 +0100

    Use jacaco 0.8.6 so that it works on Java 15.

--- services/student/src/test/java/op29sem58/student/CourseLecturesTest.java
+++ services/student/src/test/java/op29sem58/student/CourseLecturesTest.java
@@ -3,7 +3,9 @@ package op29sem58.student;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class CourseLecturesTest {
     @Test
     public void constructorTest() {
