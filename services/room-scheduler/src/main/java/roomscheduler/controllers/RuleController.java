1fa991a6239637e023f8d5378e5296d55b660317
michelbulten <m.j.g.bulten@student.tudelft.nl>
2020-12-08 14:37:51 +0100

    Making the getAllRules endpoint more compact.

--- services/room-scheduler/src/main/java/roomscheduler/controllers/RuleController.java
+++ services/room-scheduler/src/main/java/roomscheduler/controllers/RuleController.java
@@ -12,8 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -32,8 +31,7 @@ public class RuleController {
     }
 
     @PostMapping(path = "/createRule") // Map ONLY POST Requests
-    public @ResponseBody
-    String addNewRule(@RequestBody Rule r) throws IOException {
+    public @ResponseBody String addNewRule(@RequestBody Rule r) throws IOException {
         ruleRepository.saveAndFlush(r);
         return "Saved rule";
     }
@@ -43,16 +41,9 @@ public class RuleController {
      *
      * @return Array List with all rules
      */
-    @GetMapping(path = "/allrules")
-    public @ResponseBody
-    ArrayList<Rule> getAllRules() {
-        Iterable<Rule> rules = ruleRepository.findAll();
-        ArrayList<Rule> res = new ArrayList<>();
-        Iterator<Rule> iterator = rules.iterator();
-        while (iterator.hasNext()) {
-            res.add(iterator.next());
-        }
-        return res;
+    @GetMapping(path = "/allRules")
+    public @ResponseBody List<Rule> getAllRules() {
+        return ruleRepository.findAll();
     }
 
     /**
@@ -62,8 +53,7 @@ public class RuleController {
      * @return message
      */
     @PutMapping(path = "/modifyRule")
-    public @ResponseBody
-    String editRule(@RequestBody Rule r) {
+    public @ResponseBody String editRule(@RequestBody Rule r) {
         Optional<Rule> rule = ruleRepository.findById(r.getIdrules());
         if (rule.isPresent()) {
             rule.get().setName(r.getName());
@@ -82,8 +72,7 @@ public class RuleController {
      * @return message
      */
     @DeleteMapping(path = "/deleteRule/{id}")
-    public @ResponseBody
-    String deleteRule(@PathVariable int id) {
+    public @ResponseBody String deleteRule(@PathVariable int id) {
         Optional<Rule> rule = ruleRepository.findById(id);
         if (rule.isPresent()) {
             ruleRepository.deleteById(id);
