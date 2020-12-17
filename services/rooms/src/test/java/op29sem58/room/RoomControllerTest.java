5e0a079e3a38ad5778bdb61592957e81cffbb90d
Ee Xuan <e.x.tan@student.tudelft.nl>
2020-12-17 16:01:52 +0100

    fix remaining tests

--- services/rooms/src/test/java/op29sem58/room/RoomControllerTest.java
+++ services/rooms/src/test/java/op29sem58/room/RoomControllerTest.java
@@ -13,6 +13,7 @@ import op29sem58.room.controllers.RoomController;
 import op29sem58.room.entities.Room;
 import org.hamcrest.Matchers;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -28,6 +29,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -52,6 +54,11 @@ class RoomControllerTest {
     public void setUp() throws Exception {
         mockMvc = MockMvcBuilders.standaloneSetup(roomController).build();
     }
+    @BeforeAll
+    public static void mockAuthorization() {
+        MockedStatic<Authorization> mockedAuth = Mockito.mockStatic(Authorization.class);
+        mockedAuth.when(() -> Authorization.authorize("token", "Admin")).thenReturn(true);
+    }
 
     final transient Gson gson = new GsonBuilder().create();
     final transient String createRoom = "/createRoom";
@@ -62,8 +69,6 @@ class RoomControllerTest {
      */
     @Test
     public void saveRoomTest() throws Exception {
-        MockedStatic<Authorization> mockedAuth = Mockito.mockStatic(Authorization.class);
-        mockedAuth.when(() -> Authorization.authorize("token", "Admin")).thenReturn(true);
 
         String requestJson = gson.toJson(new Room("tz90", 100));
         this.mockMvc.perform(
@@ -92,14 +97,14 @@ class RoomControllerTest {
     public void modifyRoomTest() throws Exception {
         Room room = new Room("room_2", 50);
         String requestJson = gson.toJson(room);
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/createRoom")
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/createRoom").header("Authorization", "Bearer token")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(requestJson))
                 .andExpect(status().isOk())
                 .andDo(print());
 
         String req = "/room/" + "1";
-        this.mockMvc.perform(MockMvcRequestBuilders.get(req))
+        this.mockMvc.perform(MockMvcRequestBuilders.get(req).header("Authorization", "Bearer token"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("room_2"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.capacity").value(50));
@@ -109,12 +114,12 @@ class RoomControllerTest {
         room.setCapacity(350);
         String requestJson2 = gson.toJson(room);
 
-        this.mockMvc.perform(MockMvcRequestBuilders.put("/modifyRoom")
+        this.mockMvc.perform(MockMvcRequestBuilders.put("/modifyRoom").header("Authorization", "Bearer token")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(requestJson2)).andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string("Changed Room"));
 
-        this.mockMvc.perform(MockMvcRequestBuilders.get(req))
+        this.mockMvc.perform(MockMvcRequestBuilders.get(req).header("Authorization", "Bearer token"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("room_3"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.capacity").value(350));
@@ -127,14 +132,15 @@ class RoomControllerTest {
      */
     @Test
     public void countTest() throws Exception {
+
         for (int i = 0; i < 8; i++) {
             Room room = new Room("room" + i, i + 5);
-            this.mockMvc.perform(MockMvcRequestBuilders.post(createRoom)
+            this.mockMvc.perform(MockMvcRequestBuilders.post(createRoom).header("Authorization", "Bearer token")
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(gson.toJson(room))).andExpect(status().isOk());
         }
 
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/countRooms"))
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/countRooms").header("Authorization", "Bearer token"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.is(8)));
     }
@@ -147,12 +153,12 @@ class RoomControllerTest {
     public void allRoomsTest() throws Exception {
         for (int i = 0; i < 2; i++) {
             Room room = new Room("room" + i, i + 5);
-            this.mockMvc.perform(MockMvcRequestBuilders.post(createRoom)
+            this.mockMvc.perform(MockMvcRequestBuilders.post(createRoom).header("Authorization", "Bearer token")
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(gson.toJson(room))).andExpect(status().isOk());
         }
 
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/allrooms"))
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/allrooms").header("Authorization", "Bearer token"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value("1"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value("2"))
@@ -170,19 +176,19 @@ class RoomControllerTest {
     public void deleteTest() throws Exception {
         for (int i = 0; i < 2; i++) {
             Room room = new Room("room" + i, i + 5);
-            this.mockMvc.perform(MockMvcRequestBuilders.post(createRoom)
+            this.mockMvc.perform(MockMvcRequestBuilders.post(createRoom).header("Authorization", "Bearer token")
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(gson.toJson(room))).andExpect(status().isOk());
         }
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/countRooms"))
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/countRooms").header("Authorization", "Bearer token"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.is(2)));
 
         String req = "/deleteRoom/" + "2";
-        this.mockMvc.perform(MockMvcRequestBuilders.delete(req))
+        this.mockMvc.perform(MockMvcRequestBuilders.delete(req).header("Authorization", "Bearer token"))
                 .andExpect(status().isOk());
 
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/countRooms"))
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/countRooms").header("Authorization", "Bearer token"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.is(1)));
     }
