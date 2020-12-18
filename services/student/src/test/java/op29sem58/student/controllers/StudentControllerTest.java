8d7e220a1e181f9c0f428e92c0f141243d47dfdd
Alex Maat <a.maat@student.tudelft.nl>
2020-12-18 19:21:04 +0100

    Removed commented-out code

--- services/student/src/test/java/op29sem58/student/controllers/StudentControllerTest.java
+++ services/student/src/test/java/op29sem58/student/controllers/StudentControllerTest.java
@@ -1,11 +1,9 @@
 package op29sem58.student.controllers;
 
 import static org.hamcrest.Matchers.hasSize;
-// import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-// import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -15,15 +13,12 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import op29sem58.student.StudentService;
-// import op29sem58.student.communication.ServerCommunication;
 import op29sem58.student.communication.adapters.LocalDateTimeAdapter;
 import op29sem58.student.communication.authorization.Authorization;
 import op29sem58.student.communication.authorization.Role;
 import op29sem58.student.database.entities.Student;
-// import op29sem58.student.local.entities.Course;
 import op29sem58.student.local.entities.UserPreference;
 import org.junit.jupiter.api.BeforeAll;
-// import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
@@ -109,40 +104,4 @@ public class StudentControllerTest {
             .content(this.gson.toJson(newPreference))
         ).andExpect(status().isOk());
     }
-    
-    // @Test
-    // public void assignStudentsUntilTest() throws Exception {
-    //     List<Student> students = new ArrayList<>();
-    //     students.add(new Student(
-    //         "student0",
-    //         LocalDateTime.now(),
-    //         false
-    //     ));
-    //     this.initializeStudents(students);
-
-    //     List<Course> courses = new ArrayList<>();
-    //     for (int i = 0; i < 3; i++) {
-    //         courses.add(new Course(
-    //             i,
-    //             new int[]{i}, 
-    //             "teacher",
-    //             "Course " + i,
-    //             "cse" + i,
-    //             1)
-    //         );
-    //     }
-
-    //     ServerCommunication serverCommMock = Mockito.mock(ServerCommunication.class);
-    //     when(serverCommMock.getAllCourses("Bearer admin"))
-    //         .thenReturn(courses);
-    //     // when(serverCommMock.getUserId("Bearer student"))
-    //     //     .thenReturn("student0");
-
-    //     this.mockMvc.perform(
-    //         post("/assignStudentsUntil")
-    //         .content(this.gson.toJson("2020-02-03T13:00:00"))
-    //         .contentType(APPLICATION_JSON)
-    //         .header("Authorization", "Bearer admin")
-    //     ).andExpect(status().isOk()).andDo(print());
-    // }
 }
