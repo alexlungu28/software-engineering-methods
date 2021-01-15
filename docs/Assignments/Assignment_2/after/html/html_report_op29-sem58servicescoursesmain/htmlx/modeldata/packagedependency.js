e4e567e33d40362292a15b4cf3b21259cf4d8220
Alex Maat <a.maat@student.tudelft.nl>
2021-01-15 17:25:27 +0100

    Assignment 2

---
+++ docs/Assignments/Assignment_2/after/html/html_report_op29-sem58servicescoursesmain/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,17 @@
+var matrix = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,1,1,0,1],[2,0,0,1,0]]
+var packages = [{
+"name": " project.op29sem58.courses.database.entities", "color": " #3182bd"
+}
+,{
+"name": " project.op29sem58.courses.communication.authorization", "color": " #6baed6"
+}
+,{
+"name": " project.op29sem58.courses.database.repos", "color": " #9ecae1"
+}
+,{
+"name": " project.op29sem58.courses", "color": " #c6dbef"
+}
+,{
+"name": " project.op29sem58.courses.buildpattern", "color": " #e6550d"
+}
+];
---
+++ docs/Assignments/Assignment_2/after/html/html_report_op29-sem58servicesroom-schedulermain/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,20 @@
+var matrix = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[3,4,4,3,0,0],[0,0,0,0,0,0]]
+var packages = [{
+"name": " roomscheduler.communication", "color": " #3182bd"
+}
+,{
+"name": " roomscheduler.entities", "color": " #6baed6"
+}
+,{
+"name": " roomscheduler.repositories", "color": " #9ecae1"
+}
+,{
+"name": " roomscheduler.communication.authorization", "color": " #c6dbef"
+}
+,{
+"name": " roomscheduler.controllers", "color": " #e6550d"
+}
+,{
+"name": " roomscheduler", "color": " #fd8d3c"
+}
+];
