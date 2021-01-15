a6c7f10ac88e29abe5a662333d6122cd710d6971
Alex Maat <a.maat@student.tudelft.nl>
2021-01-15 15:03:34 +0100

    Moved report for assignment 1 and added code mr reports

---
+++ docs/Assignments/Assignment_2/CodeMR/op29-sem58/html/html_report_op29-sem58servicesauthenticationmain/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,11 @@
+var matrix = [[0,1,3],[0,0,0],[0,1,0]]
+var packages = [{
+"name": " op29sem58.authentication", "color": " #3182bd"
+}
+,{
+"name": " op29sem58.authentication.roles", "color": " #6baed6"
+}
+,{
+"name": " op29sem58.authentication.users", "color": " #9ecae1"
+}
+];
