03b96a106d6b31e5123ef82d2f72dd910453fb53
Luciano Carlo Hagemans <l.c.hagemans@student.tudelft.nl>
2021-01-20 22:33:20 +0100

    Fixed checkstyle and pmd

--- services/courses/src/test/java/project/op29sem58/courses/LectureBuilderTest.java
+++ services/courses/src/test/java/project/op29sem58/courses/LectureBuilderTest.java
@@ -1,26 +1,23 @@
 package project.op29sem58.courses;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import project.op29sem58.courses.buildpattern.Builder;
-import project.op29sem58.courses.buildpattern.LectureBuilder;
 import project.op29sem58.courses.buildpattern.Director;
+import project.op29sem58.courses.buildpattern.LectureBuilder;
 import project.op29sem58.courses.database.entities.Course;
 import project.op29sem58.courses.database.entities.Lecture;
 
-import java.time.LocalDate;
 
 public class LectureBuilderTest {
 
-    private Builder lb;
+    private transient Builder lb;
 
     @BeforeEach
-    void beforeAll(){
+    void beforeAll() {
         lb = new LectureBuilder();
         lb.setNumberOfTimeslots(2);
         lb.setDate(LocalDate.now());
@@ -29,14 +26,14 @@ public class LectureBuilderTest {
 
 
     @Test
-    void TestFirstYearLecture(){
+    void testFirstYearLecture() {
         Director.constructFirstYear(lb);
         Lecture lecture = lb.build();
         assertEquals(lecture.getMinNoStudents(), 500);
     }
 
     @Test
-    void TestSecondYearLecture(){
+    void testSecondYearLecture() {
         Director.constructSecondYear(lb);
         Lecture lecture = lb.build();
         assertEquals(lecture.getMinNoStudents(), 350);
